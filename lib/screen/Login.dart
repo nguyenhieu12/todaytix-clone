f912e40664543562bd95735651d87e88bfb24352
nguyenhieu12 <vailinhhon098@gmail.com>
2022-09-29 17:29:18 +0700

    complete button in login/signup screen

--- lib/screen/Login.dart
+++ lib/screen/Login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:auth_buttons/auth_buttons.dart';
 
 import 'Signup.dart';
 
@@ -8,82 +10,192 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ClipPath(
-        clipper: LoginScreenClipPath(),
-        child: Container(
-          color: Colors.red,
-          height: 350,
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: EdgeInsets.only(top: 65),
-            child: Center(
-              child: Column(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.55,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.pink.shade300
-                    ),
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 15),
-                      child: Row(
-                        children: [
-                          OutlinedButton(
-                            onPressed: () {},
-                            child: Text('Log in',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.black
-                                )
-                            ),
-                            style: OutlinedButton.styleFrom(
-                              shape: StadiumBorder(),
-                              backgroundColor: Colors.white,
-                            ),
-                          ),
-                          SizedBox(width: 25),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) => const SignupScreen()));
-                            },
-                            child: Text('Sign up',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.white
+      body: Column(
+        children: [
+          ClipPath(
+            clipper: LoginScreenClipPath(),
+            child: Container(
+              height: 350,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [
+                    0,
+                    0.4
+                  ],
+                  colors: [
+                    Color.fromARGB(100, 197, 0, 35),
+                    Colors.red
+                  ]
+                )
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(top: 65),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.55,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          color: Color.fromARGB(100, 90, 34, 34)
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 15),
+                          child: Row(
+                            children: [
+                              OutlinedButton(
+                                onPressed: () {},
+                                child: Text('Log in',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.black
+                                    )
+                                ),
+                                style: OutlinedButton.styleFrom(
+                                  shape: StadiumBorder(),
+                                  backgroundColor: Colors.white,
+                                ),
+                              ),
+                              SizedBox(width: 25),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.push(context,
+                                      MaterialPageRoute(builder: (context) => const SignupScreen()));
+                                },
+                                child: Text('Sign up',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white
+                                  )
+                                ),
+                                style: OutlinedButton.styleFrom(
+                                  shape: StadiumBorder(),
+                                  backgroundColor: Colors.transparent,
+                                ),
                               )
-                            ),
-                            style: OutlinedButton.styleFrom(
-                              shape: StadiumBorder(),
-                              backgroundColor: Colors.transparent,
-                            ),
-                          )
-                        ],
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  const Text('Welcome',
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      )
-                  ),
-                  const Text('Back',
-                      style: TextStyle(
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      )
-                  ),
-                ],
+                      SizedBox(height: 20),
+                      const Text('Welcome',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )
+                      ),
+                      const Text('Back',
+                          style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          )
+                      ),
+                    ],
+                  )
+                ),
               )
             ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2,
+            decoration: BoxDecoration(
+                color: Colors.white
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/google_logo.png',
+                          width: 28,
+                          height: 28,
+                        ),
+                        SizedBox(width: 50),
+                        Text(
+                          'Continue with Google',
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.black
+                          ),
+                        )
+                      ],
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      shape: StadiumBorder(),
+                      backgroundColor: Color.fromARGB(100, 192, 192, 192),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/email_logo.png',
+                          width: 35,
+                          height: 35,
+                        ),
+                        SizedBox(width: 50),
+                        Text(
+                          'Continue with email',
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.black
+                          ),
+                        )
+                      ],
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      shape: StadiumBorder(),
+                      backgroundColor: Color.fromARGB(100, 192, 192, 192),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/facebook_logo.png',
+                          width: 45,
+                          height: 45,
+                        ),
+                        SizedBox(width: 44),
+                        Text(
+                          'Continue with Facebook',
+                          style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.black
+                          ),
+                        )
+                      ],
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      shape: StadiumBorder(),
+                      backgroundColor: Color.fromARGB(100, 192, 192, 192),
+                    ),
+                  ),
+                )
+
+              ],
+            ),
           )
-        ),
+        ],
       ),
     );
   }
