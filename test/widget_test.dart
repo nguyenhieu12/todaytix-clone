200fbf1595232e9183a8f34c4e353ce71c97e942
nguyenhieu12 <vailinhhon098@gmail.com>
2022-10-06 12:43:04 +0700

    connect to firebase

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
-import 'package:todaytix_official/main.dart';
+import 'package:flutter_project/main.dart';
 /*import 'package:flutter_project/main.dart';*/
 
 void main() {
