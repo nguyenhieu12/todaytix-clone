cd5ec0d7030d00416d3e03e939dbfd6776a7152c
NguyenKhaiHoan <Hoanbang1234@gmail.com>
2022-10-28 12:08:46 +0700

    update

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -11,7 +11,6 @@ import '../controllers/movies_controller.dart';
 import '../model/movie.dart';
 import '../model/review.dart';
 import '../model/utils.dart';
-import '../widgets/details_appbar.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({
@@ -26,12 +25,280 @@ class DetailsScreen extends StatelessWidget {
     double mWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          DetailsAppBar(
-            movie: movie,
-          ),
-        ],
+      body: Container(
+        child: Stack(
+          // physics: BouncingScrollPhysics(),
+          children: [
+            Stack(
+              children: [
+                InkWell(
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height * (1 - 0.63),
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.network(
+                      Api.imageBaseUrl + movie.backdropPath,
+                      width: Get.width,
+                      fit: BoxFit.cover,
+                      loadingBuilder: (_, __, ___) {
+                        if (___ == null) return __;
+                        return FadeShimmer(
+                          width: Get.width,
+                          height: 250,
+                          highlightColor: const Color(0xff22272f),
+                          baseColor: const Color(0xff20252d),
+                        );
+                      },
+                      errorBuilder: (_, __, ___) => const Align(
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.broken_image,
+                          size: 250,
+                        ),
+                      ),
+                    ),
+                    /*child: CachedNetworkImage(
+                          imageUrl: info.backdrops,
+                          fit: BoxFit.cover,
+                        ),*/
+                  ),
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, top: 10, right: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CreateIcons(
+                          onTap: () => Navigator.pop(context),
+                          child: const Icon(
+                            CupertinoIcons.arrow_left,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                              ),
+                              child: ClipOval(
+                                  child: Container(
+                                height: 40,
+                                width: 40,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.white,
+                                ),
+                                child: IconButton(
+                                  onPressed: () {
+                                    Get.put(MoviesController())
+                                        .addToWatchList(movie);
+                                  },
+                                  icon: Obx(
+                                    () => Get.put(MoviesController())
+                                            .isInWatchList(movie)
+                                        ? const Icon(
+                                            Icons.bookmark,
+                                            color: Colors.red,
+                                          )
+                                        : const Icon(
+                                            Icons.bookmark_outline,
+                                            color: Colors.black,
+                                          ),
+                                  ),
+                                ),
+                              )),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            CreateIcons(
+                              onTap: () {
+                                showModalBottomSheet(
+                                  context: context,
+                                  builder: (context) => Container(),
+                                );
+                              },
+                              child: const Icon(
+                                CupertinoIcons.share,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            BottomInfoSheet(child: [
+              Container(
+                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 35),
+                color: Colors.white,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          boxShadow: kElevationToShadow[3],
+                        ),
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.network(
+                              'https://image.tmdb.org/t/p/w500/' +
+                                  movie.posterPath,
+                              width: 120,
+                              height: 180,
+                              fit: BoxFit.cover,
+                              loadingBuilder: (_, __, ___) {
+                                if (___ == null) return __;
+                                return const FadeShimmer(
+                                  width: 110,
+                                  height: 140,
+                                  highlightColor: Color(0xff22272f),
+                                  baseColor: Color(0xff20252d),
+                                );
+                              },
+                            )),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              movie.title,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 10),
+                            const SizedBox(height: 10),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: DefaultTabController(
+                  length: 2,
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: const TabBar(
+                            indicatorWeight: 4,
+                            labelColor: Colors.red,
+                            indicatorSize: TabBarIndicatorSize.label,
+                            indicatorColor: Colors.redAccent,
+                            unselectedLabelColor: Colors.grey,
+                            tabs: [
+                              Tab(
+                                text: 'About Movie',
+                              ),
+                              Tab(text: 'Reviews'),
+                            ]),
+                      ),
+                      SizedBox(
+                        height: 400,
+                        child: TabBarView(children: [
+                          Container(
+                              margin: const EdgeInsets.only(top: 20),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'About ' + movie.title,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  ReadMoreText(
+                                    movie.overview,
+                                    trimLines: 4,
+                                    colorClickableText: Colors.black,
+                                    moreStyle: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        decoration: TextDecoration.underline),
+                                    lessStyle: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        decoration: TextDecoration.underline),
+                                    trimMode: TrimMode.Line,
+                                    trimCollapsedText: '\nRead More',
+                                    trimExpandedText: '\nShow less',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Container(
+                                    child: Column(
+                                      children: [
+                                        ListTile(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              horizontal: 0),
+                                          leading: Icon(
+                                            Icons.calendar_month_outlined,
+                                            color: Colors.redAccent,
+                                          ),
+                                          title: Text(
+                                            'Release Date',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                          subtitle: Text(
+                                              movie.releaseDate.split('-')[0]),
+                                        ),
+                                        ListTile(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              horizontal: 0),
+                                          leading: Icon(
+                                            Icons.emoji_emotions_outlined,
+                                            color: Colors.redAccent,
+                                          ),
+                                          title: Text(
+                                            'Genres',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                          subtitle:
+                                              Text(Utils.getGenres(movie)),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              )),
+                          Container()
+                        ]),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ])
+          ],
+        ),
       ),
       bottomNavigationBar: ClipRRect(
         child: BackdropFilter(
@@ -40,7 +307,7 @@ class DetailsScreen extends StatelessWidget {
             padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
             height: 100,
             decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.3),
+              color: Colors.white.withOpacity(0.85),
             ),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
